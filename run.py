838989bdd3d6f5d158b874c88e0b2ac695ba7786
zhyueqi <zhyueqi@gmail.com>
2015-07-05 20:50:12 +0800

    logger is wrong & fix  Function status

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from logging.handlers import RotatingFileHandler
 log_format = logging.Formatter('%(asctime)s% - %(levelname)s% - %(message)s%')
 log_file = 'conn_log.log'
 log_handler = RotatingFileHandler(
-    log_file, mode='a', maxBytes=1 * 1024 * 1024, backupCount=1, encoding='gbk', delay=0)
+    log_file, mode='a', maxBytes=1 * 1024 * 1024, backupCount=1, encoding='utf-8', delay=0)
 log_handler.setFormatter(log_format)
 log_handler.setLevel(logging.INFO)
 logger = logging.getLogger(log_file)
@@ -19,19 +19,28 @@ AP_host = 'http://172.30.16.53'
 def isconn():
     res = requests.get(AP_host + '/cgi-bin/rad_user_info')
     code = res.status_code
+    content = res.content
+    isLogin =  True
     if code is 200:
-        logger.info('已连接')
-        return True
-    else:
-        logger.warn('失去连接')
-        return False
+        if  content != 'not_online':
+            #logger.info('online')
+            print 'online'
+        else:
+            #logger.info('not online')
+            print 'not online'
+            isLogin = False
 
+    else:
+        #logger.warn('server error')
+        isLogin = False
+    return isLogin
 
 def login():
-    logger.info('正在拨号...')
+    #logger.info('正在拨号...')
+    print 'try login '
     postData = {
         'action': 'login',
-        'ac_id': 10,  # 10:无需mac
+        'ac_id': 10,  # 10:request without mac
         'wbaredirect': '',
         'username': '148700',
         'password': '153410',
@@ -44,7 +53,7 @@ def login():
 if __name__ == '__main__':
     import time
     while True:
-        time.sleep(15)
-        if isconn:
+        time.sleep(5)
+        if isconn():
             continue
         login()
